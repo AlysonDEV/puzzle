6a1cb00e7e7e45dfce4c2cadc4eaa953b14ef3a8
erleydo7 <reps1@discente.ifpe.edu.br>
2025-12-09 07:16:09 -0300

    Francylene tem n

--- src/routes/sobre/+page.svelte
+++ src/routes/sobre/+page.svelte
@@ -8,7 +8,7 @@
 <li>Grupo:</li>
   <li>Crislayne Lopes<br><a href="mailto:cll4@discente.ifpe.edu.br">cll4@discente.ifpe.edu.br</a></li>
   <li>Emanoel Luiz<br><a href="mailto:els31@discente.ifpe.edu.br">els31@discente.ifpe.edu.br</a></li>
-  <li>Fracylene Oliveira<br><a href="mailto:fos4@discente.ifpe.edu.br">fos4@discente.ifpe.edu.br</a></li>
+  <li>Francylene Oliveira<br><a href="mailto:fos4@discente.ifpe.edu.br">fos4@discente.ifpe.edu.br</a></li>
   <li>Hevelyn Nenes<br><a href="mailto:hnsf@discente.ifpe.edu.br">hnsf@discente.ifpe.edu.br</a></li>
   <li>Maria Beatriz<br><a href="mailto:Mbap@discente.ifpe.edu.br">Mbap@discente.ifpe.edu.br</a></li>
   <li>Rhuan Erley<br><a href="mailto:Reps1@discente.ifpe.edu.br">Reps1@discente.ifpe.edu.br</a></li>
